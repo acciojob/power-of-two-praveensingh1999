205aacec9e5108f5c963fb8b84b364c0e41cd7e3
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-12-03 04:58:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const isPowerOfTwo = (number) => {
   //your code here
+	 // number must be positive
+  if (number <= 0) return false;
+
+  // Check power of 2 using bitwise AND
+  return (number & (number - 1)) === 0;
 };
 
 const num = parseInt(prompt("Enter a number"));
